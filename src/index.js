c1b26c2f624cfb3977fb8e0a218cbbf4059357af
Vinícius Marques Zaninelo <vinicousmarqueszaninelo@gmail.com>
2024-06-06 18:46:31 -0300

    Getting started in Node.js Creating User Controller and Route System

--- src/index.js
+++ src/index.js
@@ -1,15 +1,27 @@
 const http = require('http');
-const UserController = require('./controllers/UserController')
+
+const routes = require('./routes')
 
 const server = http.createServer((req, res) => {
   console.log(`Request method: ${req.method} | Endpoint: ${req.url}`)
 
-  if(req.url === '/users' && req.method === 'GET' ) {
-    UserController.listUsers(req, res);
+
+  const route = routes.find((routeObj) => (
+    routeObj.endpoint === req.url && routeObj.method === req.method
+  ));
+
+  if (route) {
+    route.handler(req, res);
   } else {
-    res.writeHead(404, { 'Content-Type': 'text/htlm' });
-    res.end(`Cannot ${req.method} ${req.url}`); 
+    if(req.url === '/users' && req.method === 'GET' ) {
+      UserController.listUsers(req, res);
+    } else {
+      res.writeHead(404, { 'Content-Type': 'text/htlm' });
+      res.end(`Cannot ${req.method} ${req.url}`); 
+    } 
   }
+
+
 });
 
 server.listen(3000, () => console.log('listening on port 3000 in localhost:3000'));
